e2671d8ef1bdfa4f7bcc18c379fdec9064c45f53
Lucas Almeida <lucasaf@dcomp.ufs.br>
2019-07-05 01:13:24 -0300

    Correções de erros, vista em perspectiva e novas texturas. Manual acrescentado.

---
+++ CG/CMakeFiles/CG.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/lucas/ProjetoCG/CG/Bmp.cpp" "/home/lucas/ProjetoCG/CG/CMakeFiles/CG.dir/Bmp.cpp.o"
+  "/home/lucas/ProjetoCG/CG/Sphere.cpp" "/home/lucas/ProjetoCG/CG/CMakeFiles/CG.dir/Sphere.cpp.o"
+  "/home/lucas/ProjetoCG/CG/main.cpp" "/home/lucas/ProjetoCG/CG/CMakeFiles/CG.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
