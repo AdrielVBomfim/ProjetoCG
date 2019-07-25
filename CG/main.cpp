879c541eea0c3a716765ebb767dab81c66458193
João Vitor Oliveira Cordeiro <joaovoc@dcomp.ufs.br>
2019-07-24 22:22:33 -0300

    Correcao do giro para matrizes de rotacao e alteracao da textura do universo para 8k

--- CG/main.cpp
+++ CG/main.cpp
@@ -6,7 +6,7 @@
                     Lucas Almeida Francisco
 
 
-    Argumentos: 0.5 (raio da terra) 2.0 (posição x do sol) 0.3 (posicao y do sol) 0.7 (posicao z do sol)
+    Argumentos: 0.5 (raio da terra) 1 (posição x do sol) 1 (posicao y do sol) 1 (posicao z do sol)
  */
 
 #include <GL/gl.h>
@@ -30,90 +30,75 @@ irrklang::ISoundEngine* engine = irrklang::createIrrKlangDevice();
 
 Sphere *spherePtr;
 GLuint texId, texIdSun, texUniverse, texMoon;
-Sphere universe(5, 60, 60, false);
 
-//Variáveis de posição da câmera
+//Renderizacao do universo ao fundo
+Sphere universe(5, 100, 100, false);
+
+//Variaveis de posição da câmera
 float posCameraX, posCameraY, posCameraZ;
 float pontoRefX, pontoRefY, pontoRefZ;
-float teta, fi;
 float raioCamera;
+
+//Variaveis da lua
 float anguloMoon = 0.0;
-float velocidade = 0.001;
 float velocidadeLua = 0.5;
-bool decolar, pousar, turn_left, turn_right;
+
+//Variaveis de fluidez do simulador
+float velocidade = 0.5;
+double rot_min = M_PI/180;
+double rot_min_up = (M_PI/180)*2;
+float ALT_MIN;
+float ALT_MAX;
+float VEL_CRUZEIRO = 0.5;
+float VEL_MAX = 1;
+
+//Controles de pouso e decolagem
+bool decolar, pousar;
 
 //Argumento correspondente ao raio do planeta
 GLdouble arg1;
 //Argumentos correspondentes à posição x, y e z do Sol
 GLfloat luz_pontual[] = {1.0, 1.0, 1.0, 1.0};
 
-float angulo = 2*M_PI/360;
-
-float ALT_MIN;
-float ALT_MAX;
-float VEL_CRUZEIRO = 0.005;
-float VEL_MAX = 0.008;
-float COEF_GIRO = 3;
 
 void iluminar(){
-    //LU1.0
-    // no mínimo 8 fontes podem ser utilizadas
-    //(iniciadas com cor preta)
-    // número de fontes de luz afeta performance
-
-    //LUZ 0
-
-    //define características para a fonte de luz 0
-    //fonte de luz direcional (por que w==0?)
     GLfloat light0_diffuse[] = { 1.0, 1.0, 1.0, 1.0 };
     GLfloat light0_specular[] = { 1.0, 1.0, 1.0, 1.0 };
     GLfloat light0_ambient[] = { 0.8, 0.8, 0.8, 1.0 };
-    //atribui características para a fonte de luz 0
     //cor padrão: branco
     glLightfv(GL_LIGHT0, GL_POSITION, luz_pontual);
     glLightfv(GL_LIGHT0, GL_DIFFUSE, light0_diffuse);
     glLightfv(GL_LIGHT0, GL_SPECULAR, light0_specular);
     glLightfv(GL_LIGHT0, GL_AMBIENT, light0_ambient);
-
-    //"acende" cada uma das luzes configuradas
     glEnable(GL_LIGHT0);
 }
 
-//desenha esfera na posição da fonte de luz
+//Desenha o sol na posicao da fonte de luz
 void desenhar_luz(){
-
     glPushAttrib (GL_LIGHTING_BIT);
-
     GLfloat mat_diffuse[] = { 1.0, 1.0, 0.0, 1.0 };
     GLfloat mat_emission[] = { 1.0, 1.0, 0.0, 1.0 };
 
     //atribui características ao material
     glMaterialfv(GL_FRONT, GL_DIFFUSE, mat_diffuse);
     glMaterialfv(GL_FRONT, GL_EMISSION, mat_emission);
-
     glMaterialfv(GL_FRONT, GL_AMBIENT_AND_DIFFUSE, mat_diffuse);
-
     glPushMatrix();
     glTranslatef(luz_pontual[0],luz_pontual[1],luz_pontual[2]);
-
     glEnable(GL_LIGHTING);
-
     glEnable(GL_TEXTURE_2D);
     glRotatef(-90, 1, 0, 0);
     glBindTexture(GL_TEXTURE_2D, texIdSun);
     Sphere sun(0.1f, 60, 60, false);
     sun.draw();
     glBindTexture(GL_TEXTURE_2D, 0);
-
-
     glDisable(GL_LIGHTING);
-
     glPopAttrib();
     glPopMatrix();
 }
 
 
-//desenha eixos xyz
+//Desenha eixos xyz
 void desenhar_eixos(){
     glEnable(GL_LIGHTING);
     //não há efeitos de iluminação nos eixos
@@ -133,23 +118,18 @@ void desenhar_eixos(){
     glEnd(); glDisable(GL_LIGHTING);
 }
 
-//desenha objeto
+//Desenha o planeta
 void desenhar_planeta(){
     //MATERIAL
     //define características para aparência do material
-    //exercício: testar exemplos da seção
-    //Changing Material Properties, do Red Book
     //GLfloat mat_specular[] = { 1, 1, 1, 1.0 };
     GLfloat mat_diffuse[] = { 0.7, 0.7, 0.7, 1.0 };
     GLfloat mat_shininess[] = { 50.0 };
-
     glPushAttrib (GL_LIGHTING_BIT);
-
     //atribui características ao material
     //glMaterialfv(GL_FRONT, GL_SPECULAR, mat_specular);
     glMaterialfv(GL_FRONT, GL_SHININESS, mat_shininess);
     glMaterialfv(GL_FRONT, GL_DIFFUSE, mat_diffuse);
-
     glEnable(GL_TEXTURE_2D);
     glEnable(GL_LIGHTING);
     glRotatef(-90, 1, 0, 0);
@@ -157,34 +137,29 @@ void desenhar_planeta(){
     spherePtr->draw();
     glBindTexture(GL_TEXTURE_2D, 0);
     glDisable(GL_LIGHTING);
-
     glPopAttrib();
 }
 
-//desenha esfera com textura de estrelas + via láctea
+//Desenha esfera com textura de estrelas + via láctea
 void desenhar_universe(){
     glPushAttrib (GL_LIGHTING_BIT);
-
     GLfloat mat_diffuse[] = { 1.0, 0.0, 1.0, 1.0 };
     GLfloat mat_emission[] = { 1.0, 1.0, 1.0, 1.0 };
     //atribui características ao material
     glMaterialfv(GL_FRONT, GL_DIFFUSE, mat_diffuse);
     glMaterialfv(GL_FRONT, GL_EMISSION, mat_emission);
     glMaterialfv(GL_FRONT, GL_AMBIENT_AND_DIFFUSE, mat_diffuse);
-
     glPushMatrix();
-
     glEnable(GL_LIGHTING);
     glEnable(GL_TEXTURE_2D);
     glBindTexture(GL_TEXTURE_2D, texUniverse);
     universe.draw();
     glBindTexture(GL_TEXTURE_2D, 0);
     glDisable(GL_LIGHTING);
-
     glPopAttrib();
     glPopMatrix();
 }
-//desenha a lua
+//Desenha a lua
 void desenhar_moon(){
     glPushAttrib (GL_LIGHTING_BIT);
     GLfloat mat_diffuse[] = { 1.0, 1.0, 1.0, 1.0 };
@@ -208,12 +183,13 @@ void desenhar_moon(){
     glPopMatrix();
 }
 
+//Funcao que recalcula as posicoes do aviao
 void spin(void){
 
     if(decolar){
      if(raioCamera < ALT_MAX) {
          raioCamera += 0.001;
-         velocidade += 0.0001;
+         velocidade += 0.01;
          if(velocidade > VEL_CRUZEIRO) velocidade = VEL_CRUZEIRO;
      }
      else {
@@ -223,7 +199,7 @@ void spin(void){
     }if(pousar){
         if(raioCamera > ALT_MIN) {
             raioCamera -= 0.001;
-            velocidade -= 0.0001;
+            velocidade -= 0.01;
             if(velocidade < 0) velocidade = 0;
         }
         else {
@@ -233,81 +209,47 @@ void spin(void){
         }
     }
 
-    float versorDesl[] = {pontoRefX-posCameraX, pontoRefY-posCameraY, pontoRefZ-posCameraZ};
-    float vetorDirecao[] = {posCameraY*versorDesl[2]-posCameraZ*versorDesl[1],
-                            posCameraZ*versorDesl[0]-posCameraX*versorDesl[2],
-                            posCameraX*versorDesl[1]-posCameraY*versorDesl[0]};
+    // Vetor na direcao do center da camera
+    GLdouble versorDesl[] = {pontoRefX-posCameraX, pontoRefY-posCameraY, pontoRefZ-posCameraZ};
+    // Vetor na direcao do giro - produto vetorial entre o up e o gase
+    GLdouble vetDir[] = {posCameraY*versorDesl[2]-posCameraZ*versorDesl[1],
+                         posCameraZ*versorDesl[0]-posCameraX*versorDesl[2],
+                         posCameraX*versorDesl[1]-posCameraY*versorDesl[0]};
+    GLdouble fator_norm = sqrt(pow(vetDir[0],2)+pow(vetDir[1],2)+pow(vetDir[2],2));
+    vetDir[0] /= fator_norm;
+    vetDir[1] /= fator_norm;
+    vetDir[2] /= fator_norm;
 
-    float giro = COEF_GIRO*(1-(velocidade/VEL_MAX)*0.6);
-    if(turn_left){
-        versorDesl[0] += vetorDirecao[0]*giro;
-        versorDesl[1] += vetorDirecao[1]*giro;
-        versorDesl[2] += vetorDirecao[2]*giro;
-        turn_left = false;
-    }if(turn_right){
-        versorDesl[0] -= vetorDirecao[0]*giro;
-        versorDesl[1] -= vetorDirecao[1]*giro;
-        versorDesl[2] -= vetorDirecao[2]*giro;
-        turn_right = false;
-    }
+    GLdouble x = vetDir[0], y = vetDir[1], z = vetDir[2];
+    GLdouble c = cos(rot_min*velocidade), s = sin(rot_min*velocidade);
+    GLdouble mat_lin1[] = {x*x*(1-c)+c,  x*y*(1-c)-z*s,  x*z*(1-c)+y*s};
+    GLdouble mat_lin2[] = {y*x*(1-c)+z*s, y*y*(1-c)+c, y*z*(1-c)-x*s};
+    GLdouble mat_lin3[] = {x*z*(1-c)-y*s, y*z*(1-c)+x*s, z*z*(1-c)+c};
+    posCameraX = posCameraX*mat_lin1[0]+posCameraY*mat_lin1[1]+posCameraZ*mat_lin1[2];
+    posCameraY = posCameraX*mat_lin2[0]+posCameraY*mat_lin2[1]+posCameraZ*mat_lin2[2];
+    posCameraZ = posCameraX*mat_lin3[0]+posCameraY*mat_lin3[1]+posCameraZ*mat_lin3[2];
 
+    pontoRefX = pontoRefX*mat_lin1[0]+pontoRefY*mat_lin1[1]+pontoRefZ*mat_lin1[2];
+    pontoRefY = pontoRefX*mat_lin2[0]+pontoRefY*mat_lin2[1]+pontoRefZ*mat_lin2[2];
+    pontoRefZ = pontoRefX*mat_lin3[0]+pontoRefY*mat_lin3[1]+pontoRefZ*mat_lin3[2];
 
-    float fator = sqrt(versorDesl[0]*versorDesl[0]+versorDesl[1]*versorDesl[1]+versorDesl[2]*versorDesl[2]);
-    versorDesl[0] = versorDesl[0]/fator;
-    versorDesl[1] = versorDesl[1]/fator;
-    versorDesl[2] = versorDesl[2]/fator;
+    GLdouble norma = sqrt(pow(posCameraX,2)+pow(posCameraY,2)+pow(posCameraZ,2));
+    GLdouble eixo_trans[] = {posCameraX/norma, posCameraY/norma, posCameraZ/norma};
 
-    posCameraX += versorDesl[0]*velocidade;
-    posCameraY += versorDesl[1]*velocidade;
-    posCameraZ += versorDesl[2]*velocidade;
+    posCameraX = eixo_trans[0]*raioCamera;
+    posCameraY = eixo_trans[1]*raioCamera;
+    posCameraZ = eixo_trans[2]*raioCamera;
 
-    pontoRefX += versorDesl[0]*velocidade;
-    pontoRefY += versorDesl[1]*velocidade;
-    pontoRefZ += versorDesl[2]*velocidade;
+    GLdouble norma2 = sqrt(pow(pontoRefX,2)+pow(pontoRefY,2)+pow(pontoRefZ,2));
+    GLdouble eixo_trans2[] = {pontoRefX/norma2, pontoRefY/norma2, pontoRefZ/norma2};
 
-    if(posCameraX != 0) teta = atan(posCameraY/posCameraX);
-    else teta = 0;
-    float numerador = sqrt(pow(posCameraX,2)+pow(posCameraY,2));
-    if(posCameraZ != 0) fi = atan(numerador/posCameraZ);
-    else fi = 0;
+    pontoRefX = eixo_trans2[0]*raioCamera;
+    pontoRefY = eixo_trans2[1]*raioCamera;
+    pontoRefZ = eixo_trans2[2]*raioCamera;
 
-    if(posCameraX > 0 && posCameraZ < 0) fi+=M_PI;
-    if(posCameraX < 0 && posCameraZ < 0) fi+=M_PI;
-    if(posCameraX < 0 && posCameraZ > 0) fi+=2*M_PI;
-
-    if(posCameraX < 0 && posCameraY > 0) teta+=M_PI;
-    if(posCameraX < 0 && posCameraY < 0) teta+=M_PI;
-    if(posCameraX > 0 && posCameraY < 0) teta+=2*M_PI;
-
-    //std::cout << "Versor: "<< versorDesl[0]<<" "<<versorDesl[1]<<" "<<versorDesl[2] << "\n";
-//    std::cout << "-------------------\n";
-//    std::cout << "Pos XYZ transl: " << posCameraX << " " << posCameraY << " " << posCameraZ<< "\n";
-//    std::cout << "Center XYZ transl: " << pontoRefX << " " << pontoRefY << " " << pontoRefZ<< "\n";
-//    std::cout << "Teta: " << teta << " Phi: " << fi<< "\n";
-
-    posCameraX = raioCamera * sin(fi) * cos(teta);
-    posCameraY = raioCamera * sin(fi) * sin(teta);
-    posCameraZ = raioCamera * cos(fi);
-
-    if(pontoRefX != 0) teta = atan(pontoRefY/pontoRefX);
-    else teta = 0;
-    numerador = sqrt(pow(pontoRefX,2)+pow(pontoRefY,2));
-    if(pontoRefZ != 0) fi = atan(numerador/pontoRefZ);
-    else fi = 0;
-
-    if(pontoRefX > 0 && pontoRefZ < 0) fi+=M_PI;
-    if(pontoRefX < 0 && pontoRefZ < 0) fi+=M_PI;
-    if(pontoRefX < 0 && pontoRefZ > 0) fi+=2*M_PI;
-
-    if(pontoRefX < 0 && pontoRefY > 0) teta+=M_PI;
-    if(pontoRefX < 0 && pontoRefY < 0) teta+=M_PI;
-    if(pontoRefX > 0 && pontoRefY < 0) teta+=2*M_PI;
-
-//    std::cout << "Center Teta: " << teta << " Phi: " << fi<< "\n";
-
-    pontoRefX = raioCamera * sin(fi) * cos(teta);
-    pontoRefY = raioCamera * sin(fi) * sin(teta);
-    pontoRefZ = raioCamera * cos(fi);
+    std::cout << sqrt(posCameraX*posCameraX+posCameraY*posCameraY+posCameraZ*posCameraZ) << "\n";
+    std::cout << "center: " << sqrt(pontoRefX*pontoRefX+pontoRefY*pontoRefY+pontoRefZ*pontoRefZ) << "\n";
+    std::cout << "vel: " << velocidade << "\n";
 
     anguloMoon += velocidadeLua;
     if(anguloMoon >= 360)
@@ -338,9 +280,6 @@ void init(void)
     posCameraY = 0.0;
     posCameraZ = 0.0;
 
-    teta = 0.0;
-    fi = M_PI/2;
-
     pontoRefX = raioCamera;//raioCamera * cos(teta) * sin(fi + angulo);
     pontoRefY = 0.1;
     pontoRefZ = 0.1;// * cos(fi + angulo);
@@ -350,12 +289,12 @@ void init(void)
 void specialKeys(int key, int x, int y)
 {
     if(key == GLUT_KEY_UP){                 //Acelerar
-        if(raioCamera > ALT_MIN) velocidade += 0.001;
+        if(raioCamera > ALT_MIN) velocidade += 0.1;
         if(velocidade > VEL_MAX) velocidade = VEL_MAX;
     }
     else if(key == GLUT_KEY_DOWN){         //Desacelerar
-        velocidade -= 0.001;
-        if (velocidade < 0) velocidade = 0;
+        velocidade -= 0.1;
+        if (velocidade < std::numeric_limits<float>::epsilon()) velocidade = 0;
         if (velocidade == 0) pousar = true;
     }
     else if(key == GLUT_KEY_PAGE_UP){           //Decolar
@@ -365,17 +304,31 @@ void specialKeys(int key, int x, int y)
         pousar = true;
     }
     else if(key == GLUT_KEY_LEFT){          // Girar para a esquerda
-        turn_left = true;
+        GLdouble x = posCameraX, y = posCameraY, z = posCameraZ;
+        GLdouble c = cos(rot_min_up*velocidade), s = sin(rot_min_up*velocidade);
+        GLdouble mat_lin1[] = {x * x * (1 - c) + c, x * y * (1 - c) - z * s, x * z * (1 - c) + y * s};
+        GLdouble mat_lin2[] = {y * x * (1 - c) + z * s, y * y * (1 - c) + c, y * z * (1 - c) - x * s};
+        GLdouble mat_lin3[] = {x * z * (1 - c) - y * s, y * z * (1 - c) + x * s, z * z * (1 - c) + c};
+
+        pontoRefX = pontoRefX * mat_lin1[0] + pontoRefY * mat_lin1[1] + pontoRefZ * mat_lin1[2];
+        pontoRefY = pontoRefX * mat_lin2[0] + pontoRefY * mat_lin2[1] + pontoRefZ * mat_lin2[2];
+        pontoRefZ = pontoRefX * mat_lin3[0] + pontoRefY * mat_lin3[1] + pontoRefZ * mat_lin3[2];
     }
     else if(key == GLUT_KEY_RIGHT){             // Girar para a esquerda
-        turn_right = true;
+        GLdouble x = posCameraX, y = posCameraY, z = posCameraZ;
+        GLdouble c = cos(-rot_min_up*velocidade), s = sin(-rot_min_up*velocidade);
+        GLdouble mat_lin1[] = {x * x * (1 - c) + c, x * y * (1 - c) - z * s, x * z * (1 - c) + y * s};
+        GLdouble mat_lin2[] = {y * x * (1 - c) + z * s, y * y * (1 - c) + c, y * z * (1 - c) - x * s};
+        GLdouble mat_lin3[] = {x * z * (1 - c) - y * s, y * z * (1 - c) + x * s, z * z * (1 - c) + c};
+
+        pontoRefX = pontoRefX * mat_lin1[0] + pontoRefY * mat_lin1[1] + pontoRefZ * mat_lin1[2];
+        pontoRefY = pontoRefX * mat_lin2[0] + pontoRefY * mat_lin2[1] + pontoRefZ * mat_lin2[2];
+        pontoRefZ = pontoRefX * mat_lin3[0] + pontoRefY * mat_lin3[1] + pontoRefZ * mat_lin3[2];
     }
 
     glutPostRedisplay();
 }
 
-// Por que a luz fica parada durante a renderização, já que quem se
-// move na cena é a câmera?
 void display(void)
 {
     //limpeza do zbuffer deve ser feita a cada desenho da tela
@@ -391,10 +344,7 @@ void display(void)
 
     desenhar_luz();
     //desenhar_eixos();
-
-
     desenhar_planeta();
-
     desenhar_universe();
     desenhar_moon();
 
@@ -408,8 +358,7 @@ void reshape(int w, int h)
 
     glMatrixMode(GL_PROJECTION);
     glLoadIdentity();
-    //glOrtho(-3.0 * arg1, 3.0 * arg1, -3.0 * arg1, 3.0 * arg1, -3.0 * arg1, 3.0 * arg1);
-    gluPerspective(70.0, 10/8, 0.001, 10);
+    gluPerspective(70.0, (float)10/8, 0.001, 10);
 
     glMatrixMode(GL_MODELVIEW);
     glLoadIdentity();
@@ -516,7 +465,7 @@ int main(int argc, char** argv)
     texId = loadTexture("8k_earth.bmp", true);
     texIdSun = loadTexture("2k_sun.bmp", true);
     texMoon = loadTexture("2k_moon.bmp", true);
-    texUniverse = loadTexture("2k_stars_milky_way.bmp", true);
+    texUniverse = loadTexture("8k_stars_milky_way.bmp", true);
 
     glutMainLoop();
     engine->drop();
